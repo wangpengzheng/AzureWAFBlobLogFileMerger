6037df0ff110194786dd5fdbecf89c7eb2116fc6
Vincent <wangpengzheng@live.com>
2020-01-14 23:35:10 +0800

    Finish code

---
+++ AzureWAFBlobLogFileMerger/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace AzureWAFBlobLogFileMerger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        input: Console.WriteLine("Please input your working folder path. This program will only iterate json files.");
+            string inputPath = Console.ReadLine();
+
+            if (!Directory.Exists(inputPath))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Your input path : {inputPath} not found, please enter again.");
+                Console.ResetColor();
+                goto input;
+            }
+
+            try
+            {
+                AppendAndCopyFiles appendAndCopyFiles = new AppendAndCopyFiles(inputPath);
+                var outputPath = appendAndCopyFiles.StartMigrate();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Finished, your migrated file path is :{outputPath}");
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error: {ex.Message} found.");
+            }
+            Console.ResetColor();
+            Console.ReadLine();
+        }
+    }
+
+    public class AppendAndCopyFiles
+    {
+        public string RootPath { get; set; }
+        public string[] fileNamesWithPath;
+
+        public AppendAndCopyFiles(string rootPath)
+        {
+            RootPath = rootPath;
+        }
+
+        public string StartMigrate()
+        {
+            string retFilePath = "";
+            string[] files = Directory.GetFiles(RootPath, "*.json", SearchOption.AllDirectories);
+
+            if (files.Length != 0)
+            {
+                retFilePath = RootPath + $@"\WholeJson-{Guid.NewGuid()}.json";
+
+                Console.WriteLine($"Create file {retFilePath}");
+                File.Create(retFilePath).Close();
+
+                using (StreamWriter sw = new StreamWriter(retFilePath, true, Encoding.UTF8))
+                {
+                    sw.Write("[");
+
+                    foreach (var file in files)
+                    {
+                        Console.WriteLine($"Reading from {file}");
+
+                        string[] contents = File.ReadAllLines(file);
+                        foreach (string line in contents)
+                        {
+                            var newline = line.Replace("}}", "}},");
+                            sw.WriteLine(newline);
+                        }
+                    }
+
+                    sw.Write("]");
+                }
+            }
+
+            return retFilePath;
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# AzureWAFBlobLogFileMerger
-Merge blob WAF log file together and transfer to a valid json
